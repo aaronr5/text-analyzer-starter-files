453ef9b5c79485b864d9ad394defb7d718589844
aaronr5 <aaronr222@hotmail.com>
2017-01-19 15:19:40 -0500

    second commit

--- app.js
+++ app.js
@@ -5,6 +5,22 @@ function getWordCount(text) {
 	return text.length;
 }
 
+function avgWordsPerSentence(text) {
+	var numOfSentences = text.match(/[.!?]+/g) ? text.match(/[.!?]+/g).length : 1;
+	var wordCount = tokenizeText(text).length;
+	return (wordCount / numOfSentences).toFixed(2);
+}
+
+function getDistinctWordCount(text) {
+	var distinctWords = [];
+	for(var i = 0; i < text.length; i++) {
+		if(distinctWords.indexOf(text[i]) === -1){
+			distinctWords.push(text[i]);
+		}
+	}
+	return distinctWords.length;
+}
+
 function tokenizeText(text) {
   return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
 }
@@ -13,8 +29,17 @@ function reportOnString(text) {
 	var token = tokenizeText(text);
 	var wordCount = getWordCount(token);
 	var avgWrdLength = token.join("").length / token.length;
-	
+	avgWrdLength = avgWrdLength.toFixed(2);
+	var distinctWordCount = getDistinctWordCount(token);
+	var avgSentenceLength = avgWordsPerSentence(text);
 
+
+	var textReport = $('.text-report');
+	textReport.find('.word-count').text(wordCount);
+	textReport.find('.unique-word-count').text(distinctWordCount);
+	textReport.find('.avg-word-length').text(avgWrdLength);
+	textReport.find('.avg-sentence-length').text(avgSentenceLength);
+	textReport.removeClass('hidden');
 }
 
 function getInput() {
