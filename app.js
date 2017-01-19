ddbb36143032d119759bf38478533e3c89bce7b0
aaronr5 <aaronr222@hotmail.com>
2017-01-19 13:51:40 -0500

    first commit

--- app.js
+++ app.js
@@ -1,1 +1,31 @@
-// your code here!
+
+
+
+function getWordCount(text) {
+	return text.length;
+}
+
+function tokenizeText(text) {
+  return text.toLowerCase().match(/\b[^\s]+\b/g).sort();
+}
+
+function reportOnString(text) {
+	var token = tokenizeText(text);
+	var wordCount = getWordCount(token);
+	var avgWrdLength = token.join("").length / token.length;
+	
+
+}
+
+function getInput() {
+	$('#user-input').submit(function(event) {
+		event.preventDefault();
+		var inputVal = $('#user-text').val();
+		reportOnString(inputVal);
+	})
+}
+
+$(function() {
+	getInput();
+})
+
